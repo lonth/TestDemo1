4805132fa80047d22f10261b5e1162d71272cdf8
longxj <21693948@qq.com>
2019-09-30 10:25:57 +0800

    first upload

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2019/9/23.
+ */
+import java.util.Date;
+public class HelloWorld{
+    public static void main(String args[]) {
+        Date date = new Date();
+        System.out.println(date.toString());
+        System.out.println("I like Java !");
+    }
+}
